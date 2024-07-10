99d99dcb9d36bb6a9d2b54aca75cd503a3c2a4da
Isaac Orraiz <isaacorraiz@gmail.com>
2024-07-10 12:13:52 -0400

    fixed sentry config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "jsm-x9",
-    project: "javascript-react"
-  }), sentryVitePlugin({
-    org: "isaac-orraiz",
-    project: "iphone-website"
-  })],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "isaac-orraiz",
+      project: "javascript-react",
+    }),
+    sentryVitePlugin({
+      org: "isaac-orraiz",
+      project: "iphone-website",
+    }),
+  ],
 
   build: {
-    sourcemap: true
-  }
-})
+    sourcemap: true,
+  },
+});
